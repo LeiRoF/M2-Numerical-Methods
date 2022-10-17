86d19e72f9f9924b6704acf998e09301f061b549
Vincent (Leirof) <vince.lrf@gmail.com>
2022-10-17 19:38:52 +0200

    Fixed animation & atom density

--- animation-lines.py
+++ animation-lines.py
@@ -5,32 +5,42 @@ import time
 from LRFutils import progress
 from matplotlib.colors import Colormap
 from numpy import *
+from matplotlib.lines import Line2D
 
 def generate(evolution, monomer, free, stuk, island, occuped_space, save_as = None, plot=False, verbose = False, axis:int=0):
 
-    fig = plt.figure(figsize=(10, 5))
+    fig = plt.figure(figsize=(40, 20))
     ax1 = fig.add_subplot(1, 2, 1)
     ax2 = fig.add_subplot(1, 2, 2)
 
     i=0
     im = ax1.imshow(evolution[0], interpolation='none', aspect='auto', vmin=0, vmax=amax(evolution), cmap="inferno")
-    ax2.set_xlim(0, len(evolution))
-    ax2.set_ylim(0, max(max(monomer), max(free), max(stuk), max(island)))
+    ax2.set_ylim(0, len(evolution))
+    line1 = Line2D([], [], label="Monomers", color="red")
+    line2 = Line2D([], [], label="Free monomers", color="blue")
+    line3 = Line2D([], [], label="Stuck monomers", color="green")
+    line4 = Line2D([], [], label="Occuped space", color="black")
+    line5 = Line2D([], [], label="Islands", color="orange")
+    ax2.add_line(line1)
+    ax2.add_line(line2)
+    ax2.add_line(line3)
+    ax2.add_line(line4)
+    ax2.legend(handles=[line1, line2, line3, line4], loc=2)
+    ax2.grid()
+
 
     a = progress.Bar(max=len(evolution), prefix="Generating animation")
     def updatefig(i):
         if verbose : print(f"🎞️ Generating animation... Step: {i+1}/{len(evolution)} ({(i+1)/len(evolution)*100:.0f} %)", end="\r")
         im.set_array(evolution[i])
-        ax2.plot(i, monomer[i], 'ro', label="Monomers")
-        ax2.plot(i, free[i], 'go', label="Free monomers")
-        ax2.plot(i, stuk[i], 'bo', label="Stuck monomers")
-        ax2.plot(i, occuped_space[i], 'ko', label="Occuped space")
-        ax2.plot(i, island[i], 'yo', label="Islands")
+        line1.set_data(arange(i), monomer[:i])
+        line2.set_data(arange(i), free[:i])
+        line3.set_data(arange(i), stuk[:i])
+        line4.set_data(arange(i), occuped_space[:i])
+        line5.set_data(arange(i), island[:i])
         a(i+1)
         return im,
     ani = animation.FuncAnimation(fig, updatefig, range(len(evolution)), blit=True)
-    ax2.grid()
-    ax2.legend()
 
     # __________________________________________________
     # Saving animation
